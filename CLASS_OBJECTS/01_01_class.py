af85f0dc9eee2f356ef808a13579aa93635feb3b
ashwinirj <ashwinij859@gmail.com>
2024-08-28 14:43:55 +0530

    Update

---
+++ CLASS_OBJECTS/01_01_class.py
@@ -0,0 +1,15 @@
+class Employee:
+
+    # name = "Krishna"
+    salary = 1500000   # class instance attributes
+    language = "Python"
+
+
+Ashu = Employee()
+Ashu.name = " Ashwini "  # object/instance attributes
+Ashu.language = "Java "
+print(Ashu.name, Ashu.salary, Ashu.language)
+
+Deepak = Employee()
+Deepak.name = "Deepak PS"
+print(Deepak.name, Deepak.salary, Deepak.language)
--- CLASS_OBJECTS/03_self_ex.py
+++ CLASS_OBJECTS/03_self_ex.py
@@ -1,4 +1,33 @@
 class Employee:
-    def __init__(self) -> None:
-        pass
-    
+
+    salary = 2000000
+    language = "Py"
+    def __init__(self):
+        print("I am init method, being called whenever an object of this Employee class is getting created")
+
+class Engineer:
+    def __init__(self):
+        print("This is engineers squad")
+
+class Coder(Employee,Engineer):
+
+    def __init__(self):
+        super().__init__()
+        print("I am Coder")
+
+#    name = "Krishna"
+#    salary = 1500000   # class instance attributes
+#    language = "Python"
+
+
+# Ashu = Employee()
+# Ashu.name = " Ashwini "  # object/instance attributes
+# Ashu.language = "Java "
+# print(Ashu.name, Ashu.salary, Ashu.language)
+
+# Deepak = Employee()
+# Deepak.name = "Deepak PS"
+# print(Deepak.name, Deepak.salary, Deepak.language)
+
+# Dee = Engineer()
+Deee = Coder()
